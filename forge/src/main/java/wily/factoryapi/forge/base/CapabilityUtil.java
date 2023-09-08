430aaf7076c63c3d7718c1bb9975a7c45a8b0f73
Wilyicaro <icarokauanbonfim@hotmail.com>
2023-09-08 01:54:57 -0300

    Release 1.20-1.1.2 -Fixed automatic IFactoryStorage block entities capabilities registry on Forge

--- forge/src/main/java/wily/factoryapi/forge/base/CapabilityUtil.java
+++ forge/src/main/java/wily/factoryapi/forge/base/CapabilityUtil.java
@@ -12,10 +12,10 @@ public class CapabilityUtil {
     private static final List<Capability<?>> CAPABILITIES = List.of(ForgeCapabilities.FLUID_HANDLER,ForgeCapabilities.FLUID_HANDLER_ITEM,ForgeCapabilities.ITEM_HANDLER, ForgeCapabilities.ENERGY,FactoryCapabilities.CRAFTY_ENERGY);
     public static  Storages.Storage<? extends IPlatformHandlerApi<?>> capabilityToStorage(Capability<?> capability){
         int index = CAPABILITIES.indexOf(capability);
-        return index >= Storages.STORAGES.size() ? null : Storages.STORAGES.get(CAPABILITIES.indexOf(capability));
+        return index >= Storages.STORAGES.size() || index < 0 ? null : Storages.STORAGES.get(CAPABILITIES.indexOf(capability));
     }
     public static Capability<?> storageToCapability(Storages.Storage<?> capability){
         int index = Storages.STORAGES.indexOf(capability);
-        return index >= CAPABILITIES.size() ? null : CAPABILITIES.get(Storages.STORAGES.indexOf(capability));
+        return index >= CAPABILITIES.size() || index < 0 ? null : CAPABILITIES.get(Storages.STORAGES.indexOf(capability));
     }
 }
