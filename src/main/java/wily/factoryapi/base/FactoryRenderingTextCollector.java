53a59e6b8d40c2b9caffff4a629b4a1aac72281d
Jab125 <67534807+Jab125@users.noreply.github.com>
2026-01-03 01:07:57 +1100

    try fix AbstractButtonMixin

---
+++ src/main/java/wily/factoryapi/base/FactoryRenderingTextCollector.java
@@ -0,0 +1,9 @@
+//? if >=1.21.11 {
+package wily.factoryapi.base;
+
+import net.minecraft.client.gui.GuiGraphics;
+
+public interface FactoryRenderingTextCollector {
+	GuiGraphics getGuiGraphics();
+}
+//?}
