71e28f02d59b65e74bc4dd48c6f351c5c094f4ec
Jab125 <67534807+Jab125@users.noreply.github.com>
2026-01-03 22:57:29 +1100

    destroy everything for 26.1

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("dev.architectury.loom") version "1.13.+" apply false
+    //id("dev.architectury.loom") version "1.13.+" apply false
+    id("net.fabricmc.fabric-loom") version "1.14.+" apply false
+    id("net.fabricmc.fabric-loom-remap") version "1.14.+" apply false
 }
 
 stonecutter active "1.20.4-fabric" /* [SC] DO NOT EDIT */
